17f0893c322053733339960dc3db47cd3273300e
Benjamin Mosior <bemosior@gmail.com>
2016-02-10 00:07:25 -0500

    Use faker in tests.

--- test/controllers/splinters_controller_test.rb
+++ test/controllers/splinters_controller_test.rb
@@ -7,12 +7,12 @@ class SplintersControllerTest < ActionController::TestCase
   end
 
   test "should post create" do
-    post :create, splinter: {title: "test", url: "http://www.github.com", image: "abc"}
+    post :create, splinter: {title: Faker::Company.catch_phrase, url: Faker::Internet.url, image: "abc"}
     assert_response :redirect
   end
 
   test "should get show" do
-    splinter = Splinter.new(id: 1, title: "test", url: "http://www.github.com", image: "abc")
+    splinter = Splinter.new(id: 1, title: Faker::Company.catch_phrase, url: Faker::Internet.url, image: "abc")
     splinter.save
     get(:show, {'id' => "1"})
     assert_response :success
