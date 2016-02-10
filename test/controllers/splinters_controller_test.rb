546e472b37639b8e017aefbb920828160f836eae
Benjamin Mosior <bemosior@gmail.com>
2016-02-09 23:34:38 -0500

    Add tests for controllers.

--- test/controllers/splinters_controller_test.rb
+++ test/controllers/splinters_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class SplintersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should post create" do
+    post :create
+    assert_response :success
+  end
 end
