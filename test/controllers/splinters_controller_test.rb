fe43001fd60ff9bd9b3738bd2bc4809d19e07b9e
Benjamin Mosior <bemosior@gmail.com>
2016-02-10 22:32:29 -0500

    Introduce rubocop changes, enhance tests.

--- test/controllers/splinters_controller_test.rb
+++ test/controllers/splinters_controller_test.rb
@@ -1,44 +1,67 @@
 require 'test_helper'
 
 class SplintersControllerTest < ActionController::TestCase
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should post create" do
-    post :create, splinter: {title: Faker::Company.catch_phrase, url: Faker::Internet.url, image: "abc"}
+  test 'should post create' do
+    post :create, splinter: {
+      title: Faker::Company.catch_phrase,
+      url: Faker::Internet.url,
+      image: 'abc'
+    }
     assert_response :redirect
   end
 
-  test "should get edit" do
-    splinter = Splinter.new(id: 1, title: Faker::Company.catch_phrase, url: Faker::Internet.url, image: "abc")
+  test 'should get edit' do
+    splinter = Splinter.new(
+      id: 1,
+      title: Faker::Company.catch_phrase,
+      url: Faker::Internet.url,
+      image: 'abc'
+    )
     splinter.save
-    get(:edit, {'id' => "1"})
+
+    get(:edit, id: 1)
     assert_response :success
+    assert_select 'input[value=?]', splinter.title
+    assert_select 'input[value=?]', splinter.url
+    assert_select 'input[value=?]', splinter.image
   end
 
-  test "should get show" do
-    splinter = Splinter.new(id: 1, title: Faker::Company.catch_phrase, url: Faker::Internet.url, image: "abc")
+  test 'should get show' do
+    splinter = Splinter.new(
+      id: 1,
+      title: Faker::Company.catch_phrase,
+      url: Faker::Internet.url,
+      image: 'abc'
+    )
     splinter.save
-    get(:show, {'id' => "1"})
+
+    get(:show, id: 1)
     assert_response :success
     assert_select 'dd', splinter.title
     assert_select 'dd', splinter.url
     assert_select 'dd', splinter.image
   end
 
-  test "should get index" do
+  test 'should get index' do
     splinters = []
     (1..5).each do |id|
-      splinter = Splinter.new(id: id, title: Faker::Company.catch_phrase, url: Faker::Internet.url, image: "abc")
+      splinter = Splinter.new(
+        id: id,
+        title: Faker::Company.catch_phrase,
+        url: Faker::Internet.url,
+        image: 'abc'
+      )
       splinter.save
       splinters.push(splinter)
     end
 
     get :index
     assert_response :success
-
     splinters.each do |splinter|
       assert_select 'td', splinter.title
       assert_select 'td', splinter.url
